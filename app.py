adb40d67487e790fdfc757238f3fc531dc6707e2
tarunrama13 <Tarun.gupta@blitzpermits.ai>
2024-08-15 11:12:22 +0530

    pysqlite3

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain.embeddings import HuggingFaceEmbeddings,OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain_community.llms.ollama import Ollama
